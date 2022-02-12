9d69099189bbdebab0de2a9f6188af5fa916bbe2
tnmy2426 <al.nahdi.tnmy@gmail.com>
2022-02-12 10:31:29 +0600

    Corporate Landing Page Development With TailwindCSS.

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+   content: ["./public/index.html"],
+   theme: {
+      fontFamily: {
+         exo: ["Exo", "sans-serif"],
+      },
+      extend: {
+         backgroundImage: {
+            "header-image": "url('./img/background01.png')",
+            "photo-bg": "url('./img/background02.png')",
+            "features-bg": "url('./img/background03.png')",
+         },
+
+         colors: {
+            "main-clr": "#ff6d6d",
+            "gray-text": "#99999a",
+         },
+      },
+
+      container: {
+         center: true,
+         padding: "1rem",
+      },
+   },
+   plugins: [],
+};
